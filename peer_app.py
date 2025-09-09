2828b8e3598751d4d09c9cf3361bdbf3afc444ad
cpoulo2 <cdpoulos@gmail.com>
2025-09-09 16:45:31 -0500

    add hint for gap

--- peer_app.py
+++ peer_app.py
@@ -301,6 +301,8 @@ with stylable_container(
     with col2:
         st.markdown('<span class="header-title">PEER - Illinois District Funding Tool</span>', unsafe_allow_html=True) # Erykah - Header title. 
 
+st.sidebar()
+
 tab0, tab1,tab2,tab3 = st.tabs(["Landing Page","District Resource Needs","Legislative View","About"]) 
 
 with tab0:
