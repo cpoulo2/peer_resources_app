1c6da36bda52c667b448087c0154e4c31403e374
cpoulo2 <cdpoulos@gmail.com>
2025-09-16 19:56:40 -0500

    Editing changes

--- peer_app.py
+++ peer_app.py
@@ -1042,11 +1042,11 @@ with tab3:
     st.subheader("About the Tool")
     st.markdown("""The PEER - Illinois District Funding Tool aims to do 3 things:
 
-**Educate:** Provide those directly affected by K-12 resource inequality with easily accessible facts about resource inequality in the State of Illinois and their school districts.
+- **Educate:** Provide those directly affected by K-12 resource inequality with easily accessible facts about resource inequality in the State of Illinois and their school districts.
 
-**Advocate:** Provide information by legislative district to pressure our political leaders to fund education.
+- **Advocate:** Provide information by legislative district to pressure our political leaders to fund education.
 
-**Organize:** Get people to join PEER IL in the fight for fully resourced schools (learn more about PEER IL and sign up below).
+- **Organize:** Get people to join PEER IL in the fight for fully resourced schools (learn more about PEER IL and sign up below).
 """,unsafe_allow_html=True)
     
     st.subheader("About PEER IL")
